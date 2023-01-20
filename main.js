29525f6541d081fb17e624a76e2c46490a120c75
rosendo <rosendc30@gmail.com>
2023-01-20 09:05:51 -0300

    finish - finalizado

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-const form = document.querySelector('#form-habits')
-const nlwSetup = new NLWSetup(form)
+const form = document.querySelector("#form-habits");
+const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-const data = {
-    run: ['01-01', '01-02', '06-01'],
-    water: ['01-01', '01-03', '01-04'],
-    food: ['01-01', '01-02', '06-01'],
-    journal: ['01-01', '01-02', '06-01'],
-    takePills: ['01-01', '01-02', '05-01'],
-}
+button.addEventListener("click", () => {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
+  const dayExists = nlwSetup.dayExists(today);
+  dayExists
+    ? alert(`Dia ${today} já existe na tabela`)
+    : nlwSetup.addDay(today);
+  return;
+});
+form.addEventListener("change", async () => {
+  await localStorage.setItem("NLWSetup@habits", JSON.stringify(nlwSetup.data));
+});
 
-nlwSetup.setData(data)
-nlwSetup.load()
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {};
+
+nlwSetup.setData(data);
+nlwSetup.load();
